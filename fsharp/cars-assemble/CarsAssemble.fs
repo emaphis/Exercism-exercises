cfc11deee2f01fd198f7a30fb6c31802c4996b21
emaphis <emaphis85@gmail.com>
2023-02-28 11:06:33 -0500

    A simplification

--- fsharp/cars-assemble/CarsAssemble.fs
+++ fsharp/cars-assemble/CarsAssemble.fs
@@ -5,9 +5,9 @@ let minutesPerHour = 60
 
 let successRate (speed: int): float =
     if speed = 0 then 0.0
-    else if speed <= 4 then 1.0
-    else if speed <= 8 then 0.9
-    else if speed = 9 then 0.8
+    elif speed <= 4 then 1.0
+    elif speed <= 8 then 0.9
+    elif speed = 9 then 0.8
     else 0.77
 
 
