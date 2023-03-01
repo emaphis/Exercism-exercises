a6458700fc01d8c7908035ebb21f5c2f26ecbcca
emaphis <emaphis85@gmail.com>
2023-03-01 02:24:46 -0500

    Simplified Panagram app.

--- fsharp/pangram/Pangram.fs
+++ fsharp/pangram/Pangram.fs
@@ -3,4 +3,4 @@ module Pangram
 let isPangram (input: string): bool =
     let letters = [ 'a' .. 'z' ]
     let lower = input.ToLower()
-    List.forall (fun (chr: char) -> lower.Contains(chr)) letters
+    List.forall (fun chr -> lower.Contains(chr.ToString())) letters
