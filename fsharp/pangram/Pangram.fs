b8dda23c5240894f4b32bf331723019ebba4fe9d
emaphis <emaphis85@gmail.com>
2023-02-28 22:51:42 -0500

    Solved Panagrams

--- fsharp/pangram/Pangram.fs
+++ fsharp/pangram/Pangram.fs
@@ -1,3 +1,6 @@
 module Pangram
 
-let isPangram (input: string): bool = failwith "You need to implement this function."
+let isPangram (input: string): bool =
+    let letters = [ 'a' .. 'z' ]
+    let lower = input.ToLower()
+    List.forall (fun (chr: char) -> lower.Contains(chr)) letters
