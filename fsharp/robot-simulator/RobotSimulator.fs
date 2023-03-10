24f14c75f9a077999c9320c9ef29265fdfdbd5e0
emaphis <emaphis85@gmail.com>
2023-03-10 13:50:30 -0500

    Updated recursive `loop` function to match library `fold ` signature Added `fold` solution to RobotSimulator

--- fsharp/robot-simulator/RobotSimulator.fs
+++ fsharp/robot-simulator/RobotSimulator.fs
@@ -38,6 +38,9 @@ let private advance robot =
         | West  -> (x-1, y)
     create robot.direction position
 
+/// Takes a robot and an instruction and "preforms"
+/// instruction returning a new robot
+///  - (A folder function)
 let private perform robot instruction =
     match instruction with
     | 'L' -> turnLeft robot
@@ -46,14 +49,23 @@ let private perform robot instruction =
     | _   -> failwith "Invalid instruction"
 
 
+// Using recursion
 let move instructions robot =
-    
+
     let list = Seq.toList(instructions)
     
-    let rec loop perform instList robot  =
+    let rec loop perform robot instList =
         match instList with
         | [] -> robot
         | inst :: rest ->
-            loop perform rest (perform robot inst)
+            loop perform (perform robot inst) rest
     
-    loop perform list robot
+    loop perform robot list
+
+
+// Using `fold`
+let move' instructions robot =
+
+    let list = Seq.toList(instructions)
+
+    List.fold perform robot list
