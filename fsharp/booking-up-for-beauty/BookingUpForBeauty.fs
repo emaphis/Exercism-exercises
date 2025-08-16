089759954ec2dbb1ca437aaba27b5f4be1669dd9
emaphis <emaphis85@gmail.com>
2025-08-15 20:30:36 -0400

    Booking for Beauty - DateTime, Basic formatting and parsing

---
+++ fsharp/booking-up-for-beauty/BookingUpForBeauty.fs
@@ -0,0 +1,27 @@
+module BookingUpForBeauty
+
+// The following line is needed to use the DateTime type
+open System
+
+
+let schedule (appointmentDateDescription: string): DateTime =
+    DateTime.Parse appointmentDateDescription
+
+
+let hasPassed (appointmentDate: DateTime): bool =
+    DateTime.Now > appointmentDate
+
+
+/// Checks appointment is between Noon to 6.
+let isAfternoonAppointment (appointmentDate: DateTime): bool =
+    appointmentDate.Hour >= 12 && appointmentDate.Hour < 18
+
+
+/// takes an appointment date and returns a description of that date and time:
+/// "You have an appointment on 3/29/2019 3:00:00 PM."
+let description (appointmentDate: DateTime): string =
+     "You have an appointment on "  + appointmentDate.ToShortDateString() + " " + appointmentDate.ToLongTimeString() + "."
+
+
+/// returns this year's anniversary date, which is September 15th
+let anniversaryDate(): DateTime = DateTime(DateTime.Now.Year, 9, 15)
