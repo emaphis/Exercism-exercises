6cf8d120e6ab4eb445442b22e4c70e8893de6c96
emaphis <emaphis85@gmail.com>
2025-09-11 21:19:35 -0400

    Karls Languages - Java - Generic Types, Lists.

---
+++ java/karls-languages/src/main/java/LanguageList.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanguageList {
+
+    private final List<String> languages = new ArrayList<>();
+
+    public boolean isEmpty() {
+        return languages.isEmpty();
+    }
+
+    public void addLanguage(String language) {
+        languages.add(language);
+    }
+
+    public void removeLanguage(String language) {
+        int idx = languages.indexOf(language);
+        if (idx > -1)
+            languages.remove(idx);
+    }
+
+    public String firstLanguage() {
+        return languages.getFirst();
+    }
+
+    public int count() {
+        return languages.size();
+    }
+
+    public boolean containsLanguage(String language) {
+        return languages.contains(language);
+    }
+
+    public boolean isExciting() {
+        return languages.contains("Java") || languages.contains("Kotlin");
+    }
+}
