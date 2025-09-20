cf233b5f46e5eef007df8ced142124a8c32a36d6
emaphis <emaphis85@gmail.com>
2025-09-20 13:17:25 -0400

    Refactor squeaky clean create a leetCode function to handles leetcode translation Remove the now useless '!' clause.

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,27 +1,36 @@
 class SqueakyClean {
+
+    static char leetCode (char aChar) {
+        if (aChar == '0')
+            return 'o';
+        else if (aChar == '1')
+            return 'l';
+        else if (aChar == '3')
+            return 'e';
+        else if (aChar == '4')
+           return 'a';
+        else if (aChar == '7')
+            return 't';
+        return ' ';
+    }
+
     static String clean(String identifier) {
         char[] chars = identifier.toCharArray();
         StringBuilder bld = new StringBuilder();
+
         for (int i = 0; i < chars.length; i++) {
             char aChar = chars[i];
+
             if (aChar == ' ')
                 bld.append('_');
             else if (aChar == '-')
                 bld.append(Character.toUpperCase(chars[++i]));
-            else if (aChar == '0')
-                bld.append('o');
-            else if (aChar == '1')
-                bld.append('l');
-            else if (aChar == '3')
-                bld.append('e');
-            else if (aChar == '4')
-                bld.append('a');
-            else if (aChar == '7')
-                bld.append('t');
-            else if (aChar == '¡' || aChar == '!') { }
+            else if (Character.isDigit(aChar))
+                bld.append(leetCode(aChar));
             else if (Character.isAlphabetic(aChar))
                 bld.append(aChar);
         }
+
         return bld.toString();
     }
 }
