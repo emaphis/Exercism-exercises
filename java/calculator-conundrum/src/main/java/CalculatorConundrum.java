37846e56b43a6009c1f280c1e305ef6c4d62589c
emaphis <emaphis85@gmail.com>
2025-09-20 14:46:02 -0400

    Refactor Calculator Conundrum to use switch expression.

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,28 +1,24 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        int result = 0;
 
-        switch (operation) {
-            case "+":
-                result = operand1 + operand2;
-                break;
-            case "*":
-                result = operand1 * operand2;
-                break;
-            case "/":
-                try {
-                    result = operand1 / operand2;
-                } catch (ArithmeticException ex) {
-                    throw new IllegalOperationException("Division by zero is not allowed", ex);
+        // Switch expression version
+        int result =
+            switch (operation) {
+                case "+" -> operand1 + operand2;
+                case "*" -> operand1 * operand2;
+                case "/" -> {
+                    var out = 0;
+                    try {
+                        out = operand1 / operand2;
+                    } catch (ArithmeticException ex) {
+                        throw new IllegalOperationException("Division by zero is not allowed", ex);
+                    }
+                    yield out;
                 }
-                break;
-            case "":
-                throw new IllegalArgumentException("Operation cannot be empty");
-            case null:
-                throw new IllegalArgumentException("Operation cannot be null");
-            default:
-                throw new IllegalOperationException("Operation '" + operation + "' does not exist");
-        }
+                case "" -> throw new IllegalArgumentException("Operation cannot be empty");
+                case null -> throw new IllegalArgumentException("Operation cannot be null");
+                default -> throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+            };
 
         return operand1 + " " + operation + " " + operand2 + " = " + result;
     }
